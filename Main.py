6f13d9f979b1f9ef89e6a6121e6341b37713c6e9
Aldo Koestiwa <aldokoestiwa07@gmail.com>
2026-03-20 21:17:24 +0700

    4_compile bytecode

---
+++ Main.py
@@ -0,0 +1,18 @@
+import time
+start_time = time.time()
+
+print("Hello")
+print("World")
+print("Hello world")
+
+# ini adalah comment
+""" 
+    ini adalah comment
+    multiline
+
+"""
+a = 10
+print(a)
+print(time.time() - start_time, "detik")
+# compile python ke bytecode : python -m py_compile Main.py (jadi lebih cepat)
+# 
